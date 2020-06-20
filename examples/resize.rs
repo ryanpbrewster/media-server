dbc247495c5a8b8df35ec62e64ec990f2f0b7c88
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-06-20 11:54:02 -0700

    use .thumbnail

--- examples/resize.rs
+++ examples/resize.rs
@@ -1,16 +1,13 @@
-use image::imageops::FilterType;
 use image::GenericImageView;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 fn main() {
     let opts = Opts::from_args();
-    println!("opening {:?}...", opts.input);
     let mut img = image::open(&opts.input).expect("failed to open image");
-    println!("{:?}", img.dimensions());
-    img = img.resize(400, 300, FilterType::Lanczos3);
-    println!("{:?}", img.dimensions());
-    println!("saving to {:?}...", opts.output);
+    println!("original dimensions = {:?}", img.dimensions());
+    img = img.thumbnail(128, 128);
+    println!("thumbnail dimensions = {:?}", img.dimensions());
     img.save(&opts.output).expect("failed to save image");
 }
 
