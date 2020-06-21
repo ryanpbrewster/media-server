ca73ec5899e18b894b81122f0d3ccdefee2ed805
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-06-21 14:18:29 -0700

    Add a a v0/ subpath for the server routing

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,16 @@ use warp::Filter;
 async fn main() {
     env_logger::init();
 
-    let hello = warp::get()
-        .and(warp::path::end())
-        .map(|| format!("Hello, World!"));
+    let hello = warp::get().and(warp::path::end()).map(|| "ok");
 
-    warp::serve(hello).run(([127, 0, 0, 1], 9000)).await;
+    let v0 = {
+        let object = warp::path!("o" / String);
+        let get_object = object.and(warp::get()).map(|_name: String| "TODO");
+        let create_object = object.and(warp::post()).map(|_name: String| "TODO");
+
+        get_object.or(create_object)
+    };
+
+    let routes = hello.or(warp::path("v0").and(v0));
+    warp::serve(routes).run(([127, 0, 0, 1], 9000)).await;
 }
