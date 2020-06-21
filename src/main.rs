cad1fdfe46b6a5fbef2126f4820ede98ab14d326
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-06-21 13:38:42 -0700

    add warp server structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, World!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let hello = warp::get()
+        .and(warp::path::end())
+        .map(|| format!("Hello, World!"));
+
+    warp::serve(hello).run(([127, 0, 0, 1], 9000)).await;
 }
